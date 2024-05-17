8490d419b7cecaf66f7a8bede97de1757b39f820
SamratMali <samratmali6569@gmail.com>
2024-05-17 19:45:51 +0530

    server file

--- client/src/pages/Ragisterpages.jsx
+++ client/src/pages/Ragisterpages.jsx
@@ -1,54 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Style/Registerpage.scss";
 
-const Ragisterpages = () => {
+const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profileImage: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData({
+      ...formData,
+      [name]: name === "profileImage" ? files[0] : value,
+    });
+  };
+
   return (
-    <div>
-      <div className="register">
-        <div className="register_content">
-          <form className="register_content_form">
-            <input placeholder="First Name" name="firstname" required />
-            <input placeholder="Last Name" name="lastname" required />
-            <input
-              placeholder="Email Name"
-              name="email"
-              type="email"
-              required
-            />
-            <input
-              placeholder="Password"
-              name="Password"
-              type="password"
-              required
-            />
-            <input
-              placeholder="Confirm Password"
-              name="Confirm Password"
-              type="password"
-              required
-            />
+    <div className="register">
+      <div className="register_content">
+        <form className="register_content_form">
+          <input
+            placeholder="First Name"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            placeholder="Last Name"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            type="email"
+            required
+          />
+          <input
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            type="password"
+            required
+          />
+          <input
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            type="password"
+            required
+          />
 
-            <input
-              id="image"
-              type="file"
-              name="profileImage"
-              accept="image/*"
-              style={{ display: "none" }}
-              required
-            ></input>
+          <input
+            id="image"
+            type="file"
+            onChange={handleChange}
+            name="profileImage"
+            accept="image/*"
+            style={{ display: "none" }}
+            required
+          />
 
-            <label htmlFor="image">
-              <img src="/assets/addImage.png" alt="add profile photo " />
-              <p>Upload Your Photo</p>
-            </label>
+          <label htmlFor="image">
+            <img src="/assets/addImage.png" alt="add profile photo" />
+            <p>Upload Your Photo</p>
+          </label>
 
-            <button type="submit">REGISTER</button>
-          </form>
-          <a href="/login">Already have an account? Log in here</a>
-        </div>
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile"
+              style={{ maxWidth: "80px" }}
+            />
+          )}
+
+          <button type="submit">REGISTER</button>
+        </form>
+        <a href="/login">Already have an account? Log in here</a>
       </div>
     </div>
   );
 };
 
-export default Ragisterpages;
+export default RegisterPage;
