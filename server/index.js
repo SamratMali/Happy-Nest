7e1de894624829ad83409968290a355554da644b
SamratMali <samratmali6569@gmail.com>
2024-06-12 21:23:52 +0530

    new updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = requite("dotenv").config();
+const dotenv = require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
+
+console.log(process.env.MONGO_URL);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+// MONGOOSE SETUp
+
+const PORT = 3001;
+const mongoUrl = process.env.MONGO_URL;
+
+if (!mongoUrl) {
+  console.error("Error: MONGO_URL is not defined in the .env file");
+  process.exit(1);
+}
+
+mongoose
+  .connect(mongoUrl, {})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => {
+    console.error(`Error: ${err.message}`);
+    console.error(
+      "Failed to connect to MongoDB. Please check your connection string and MongoDB service."
+    );
+  });
