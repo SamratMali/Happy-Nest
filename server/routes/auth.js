f66d35532068b3c6cf6764ff9d5820ff84c7c3ed
SamratMali <samratmali6569@gmail.com>
2024-06-14 11:04:01 +0530

    auth is complete

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,5 +29,45 @@ router.post("/register", upload.single("profileImage"), async (req, res) => {
 
     // The uploaded file is available as req.file]
     const profileImage = req.file;
-  } catch (err) {}
+
+    if (!profileImage) {
+      return res.status(400).send("No file uploaded");
+    }
+
+    // path to the uploaded profile photo
+    const profileImagePath = profileImage.path;
+
+    // Check if user exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists!" });
+    }
+
+    // Hass The password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new User
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      profileImagePath,
+    });
+
+    // Save the new User
+    await newUser.save();
+
+    // Send a Successful message
+    res
+      .status(200)
+      .json({ message: "User registered Successfully!", user: newUser });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Registration Failed!", error: err.message });
+  }
 });
